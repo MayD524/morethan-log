9fe22dac9083815aa0ed660bc13899dd0822de65
May <57610348+MayD524@users.noreply.github.com>
2023-08-03 22:22:18 -0400

    fixed?

--- src/routes/Feed/MobileProfileCard.tsx
+++ src/routes/Feed/MobileProfileCard.tsx
@@ -24,7 +24,7 @@ const MobileProfileCard: React.FC<Props> = () => {
             <div className="top">{CONFIG.profile.name}</div>
             <div className="mid">{CONFIG.profile.role}</div>
             <div className="btm">{CONFIG.profile.bio}</div>
-            <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="mayd524" data-color="#5F7FFF" data-emoji=""  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#ffffff" data-coffee-color="#FFDD00" ></script>
+            <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="mayd524" data-color="#5F7FFF" data-emoji=""  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#ffffff" data-coffee-color="#FFDD00" async />
           </div>
         </div>
       </div>
